4875833ff87d79a3f7a74d43594b516b4fc23220
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-17 00:27:35 +0100

    /

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,10 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        black: {
+          DEFAULT: "#000",
+          100: "#000319",
+        },
       },
     },
   },
