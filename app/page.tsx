fc500eba1188c412a5a8444ff78c705773e8aeaf
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-17 21:12:26 +0100

    setup acernity

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 justify-center flex flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 justify-center items-center flex flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <h1 className="">Hello, porfolio</h1>
+        <Hero />
       </div>
     </main>
   );
