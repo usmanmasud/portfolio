50cacbe02788107a11f95024ecb31efec04cb9bf
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-19 13:37:53 +0100

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
 "use client";
 
 import { navItems } from "@/data";
-
 import Hero from "@/components/Hero";
-import Grid from "@/components/Grid";
 import Footer from "@/components/Footer";
-import Clients from "@/components/Clients";
-import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
@@ -17,11 +13,8 @@ const Home = () => {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
-        {/* <Grid /> */}
         <RecentProjects />
-        {/* <Clients /> */}
         <Experience />
-        {/* <Approach /> */}
         <Footer />
       </div>
     </main>
