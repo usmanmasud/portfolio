38c62c07034a28e276e77cd867d18c4dd137d73d
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-19 13:20:45 +0100

    ..

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ const Home = () => {
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
-        <Grid />
+        {/* <Grid /> */}
         <RecentProjects />
         {/* <Clients /> */}
         <Experience />
