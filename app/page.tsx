0635636c79c45012793b5990aee24c85f432e8ec
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-21 17:37:02 +0100

    update page.tsx folatingNAV

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={navItems} />
+        <FloatingNav className="" navItems={navItems} />
         <Hero />
         <RecentProjects />
         <Experience />
