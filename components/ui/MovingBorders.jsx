6aba9171c7db81fb243daa515dca5d5ab843153d
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2025-01-22 18:13:29 +0100

    moving borders

--- components/ui/MovingBorders.jsx
+++ components/ui/MovingBorders.jsx
@@ -45,6 +45,7 @@ export function Button({
           />
         </MovingBorder>
       </div>
+
       <div
         className={cn(
           "relative bg-slate-900/[0.] border border-slate-800 backdrop-blur-xl text-white flex items-center justify-center w-full h-full text-sm antialiased",
